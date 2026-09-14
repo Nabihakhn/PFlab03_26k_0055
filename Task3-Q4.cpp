28aed496a93611bf68ef9e13e2abbb1c1fa183d9
Nabiha <nabihakhurram1907@gmail.com>
2026-09-14 16:56:46 +0500

    Add files via upload

---
+++ Task3-Q4.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Student Information:\n");
+    printf("Name: \"Saim\"\n");
+    printf("Roll Number: 123\n");
+    printf("Section: A\n\n");
+    printf("Path: C:\\Users\\Student\n");
+    return 0;
+}
---
+++ task3-Q3.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/*Write a C program that takes two integers as input and displays the quotient and remainder when the
+first is divided by the second.*/
+
+int main ()
+{	
+	int num1;
+	int num2;
+	int remainder;
+	int quotient;
+    printf("Enter num1: ");
+    scanf("%u", &num1);
+    printf("Enter num2: ");
+    scanf("%u", &num2);
+    remainder = num1%num2;
+    quotient = num1/num2;
+    printf("remainder: %u\n", remainder);
+    printf("quotient: %u\n", quotient); 
+}
+
